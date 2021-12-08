73c6911a3c03f21a2059e74d01db9c587d6f89d7
Ruan Azevedo <ruan.azevedo@gmail.com>
2021-12-08 11:25:41 -0300

    ✨ Implement cart reducer and CartIcon/CartDropdown components

---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,5 @@
+import CarTActionTypes from "./cart.types";
+
+export const toggleCartHidden = () => ({
+  type: CarTActionTypes.TOGGLE_CART_HIDDEN,
+});
---
+++ src/redux/cart/cart.types.js
@@ -0,0 +1,5 @@
+const CarTActionTypes = {
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+};
+
+export default CarTActionTypes;
