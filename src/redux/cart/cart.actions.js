643b785b1f8a9bae251cc99c6c55ccc45bfc2a4b
Ruan Azevedo <ruan.azevedo@gmail.com>
2021-12-08 15:42:49 -0300

    ✨ Implement shopping cart feature

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CarTActionTypes from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CarTActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: CarTActionTypes.ADD_ITEM,
+  payload: item,
+});
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CarTActionTypes = {
   TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+  ADD_ITEM: "ADD_ITEM",
 };
 
 export default CarTActionTypes;
