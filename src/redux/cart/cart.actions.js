91490b5a37604fa288b2a3755df3b8c052e73435
Ruan Azevedo <ruan.azevedo@gmail.com>
2021-12-14 08:45:36 -0300

    ✨ Implement remove items from cart at checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,8 @@ export const addItem = (item) => ({
   type: CarTActionTypes.ADD_ITEM,
   payload: item,
 });
+
+export const clearItemFromCart = (item) => ({
+  type: CarTActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
